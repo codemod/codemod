76d117950ee2a90562eb01e883999065f8db6889
Mohamad Mohebifar <mo@codemod.com>
2025-08-30 11:04:02 -0700

    feat: add JSON language support to jssg-types

--- packages/jssg-types/src/index.d.ts
+++ packages/jssg-types/src/index.d.ts
@@ -34,6 +34,10 @@ declare module "codemod:ast-grep/langs/html" {
   export { default } from "@codemod.com/jssg-types/langs/html";
 }
 
+declare module "codemod:ast-grep/langs/json" {
+  export { default } from "@codemod.com/jssg-types/langs/json";
+}
+
 declare module "codemod:ast-grep/langs/java" {
   export { default } from "@codemod.com/jssg-types/langs/java";
 }
