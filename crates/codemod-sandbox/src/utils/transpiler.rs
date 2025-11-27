eb138d310738654983d49a6cf236806c821f9916
Mohamad Mohebifar <mo@codemod.com>
2025-11-27 11:37:20 -0800

    fix: revert to swc for transpilation

--- crates/codemod-sandbox/src/utils/transpiler.rs
+++ crates/codemod-sandbox/src/utils/transpiler.rs
@@ -1,3 +1,37 @@
+use std::sync::Arc;
+use swc_core::common::{errors::Handler, SourceMap};
+use swc_ts_fast_strip::{Mode, Options};
+struct NullEmitter;
+impl swc_core::common::errors::Emitter for NullEmitter {
+    fn emit(&mut self, _db: &mut swc_core::common::errors::DiagnosticBuilder<'_>) {}
+}
+
+#[allow(dead_code)]
+pub fn transpile(source: String, filename: String) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let cm: Arc<SourceMap> = Default::default();
+
+    let handler = Handler::with_emitter(false, false, Box::new(NullEmitter));
+
+    let code = swc_ts_fast_strip::operate(
+        &cm,
+        &handler,
+        source,
+        Options {
+            filename: Some(filename),
+            mode: Mode::StripOnly,
+            source_map: false,
+            ..Default::default()
+        },
+    )?
+    .code
+    .into_bytes();
+
+    Ok(code)
+}
+
+// TODO(mohebifar): I tried replacing swc with oxc and it works, but it doesn't support type stripping which leads to wrong positions for tracing the source code.
+
+/*
 use std::io::Error;
 use std::path::Path;
 
@@ -44,3 +78,4 @@ pub fn transpile(source: String, filename: String) -> Result<Vec<u8>, Box<dyn st
 
     Ok(output.code.into_bytes())
 }
+*/
